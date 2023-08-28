c06362ea05392436cc09a1447c400ad18287e754
Luke Hardy <luke.taylor.hardy@gmail.com>
2023-08-28 11:19:45 -0500

    first commit 8.28

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+    console.log("you got clicked");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>let's try some stuff</h2>
+      <h3>let's try some more stuff</h3>
+      <h4>how far can we go?</h4>
+      <h5>apparently farther</h5>
+      <h6>all the way down we go</h6>
+      <h6>into oblivion</h6>
+      <h6>until this is a website for ants</h6>
+      <h6>who can't read good</h6>
+      <button className="btn-secondary" onClick={handleClick}>
+        click me bish i dare you
+      </button>
+      <div>Count:{count}</div>
     </div>
   );
-}
-
-export default App;
+};
